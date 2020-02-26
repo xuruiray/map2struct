c36d6689bcd038f3c94877040ebc876f1ba56914
Helck <971661949@qq.com>
2020-02-27 01:33:18 +0800

    add array

--- bind.go
+++ bind.go
@@ -4,7 +4,6 @@ import (
 	"strconv"
 )
 
-// bindConfig 仅能解析一层配置
 func bindConfig(configMap map[string]string, result interface{}) error {
 
 	if reflect.ValueOf(result).Kind() != reflect.Ptr {
@@ -20,33 +19,65 @@ func bindConfig(configMap map[string]string, result interface{}) error {
 
 	for i := 0; i < t.NumField(); i++ {
 		tag := t.Field(i).Tag.Get("json")
+		value, ok := configMap[tag]
+		if !ok {
+			continue
+		}
+
 		switch v.Field(i).Kind() {
 		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-			res, err := strconv.ParseInt(configMap[tag], 10, 64)
+			res, err := strconv.ParseInt(value, 10, 64)
 			if err != nil {
 				return err
 			}
 			v.Field(i).SetInt(res)
 		case reflect.String:
-			v.Field(i).SetString(configMap[tag])
+			v.Field(i).SetString(value)
 		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-			res, err := strconv.ParseUint(configMap[tag], 10, 64)
+			res, err := strconv.ParseUint(value, 10, 64)
 			if err != nil {
 				return err
 			}
 			v.Field(i).SetUint(res)
 		case reflect.Float32:
-			res, err := strconv.ParseFloat(configMap[tag], 32)
+			res, err := strconv.ParseFloat(value, 32)
 			if err != nil {
 				return err
 			}
 			v.Field(i).SetFloat(res)
 		case reflect.Float64:
-			res, err := strconv.ParseFloat(configMap[tag], 64)
+			res, err := strconv.ParseFloat(value, 64)
 			if err != nil {
 				return err
 			}
 			v.Field(i).SetFloat(res)
+		case reflect.Slice:
+			var strArray []string
+			var valArray []reflect.Value
+			var valArr reflect.Value
+			elemKind := v.Field(i).Type().Elem().Kind()
+			elemType := t.Field(i).Type.Elem()
+
+			value = strings.Trim(strings.Trim(value, "["), "]")
+			strArray = strings.Split(value, ",")
+
+			switch elemKind {
+			case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+				for _, e := range strArray {
+					ee, err := strconv.ParseInt(e, 10, 64)
+					if err != nil {
+						return err
+					}
+					valArray = append(valArray, reflect.ValueOf(ee).Convert(elemType))
+				}
+			case reflect.String:
+				for _, e := range strArray {
+					valArray = append(valArray, reflect.ValueOf(e).Convert(elemType))
+				}
+			}
+
+			valArr = reflect.Append(v.Field(i), valArray...)
+			v.Field(i).Set(valArr)
 		}
 	}
 
